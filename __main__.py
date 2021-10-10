be904eb1c35be2b2d81ae8b0f49cb6c6797c2fe2
Eduardo Suarez <eduardosuarez@gmail.com>
2021-10-10 17:12:02 +0200

    Let's try some user data

--- __main__.py
+++ __main__.py
@@ -19,20 +19,42 @@ consul_server_sg = pulumi_aws.ec2.SecurityGroup(
     description='Consul Servers Security Group',
     ingress=[
         pulumi_aws.ec2.SecurityGroupIngressArgs(
-            protocol='tcp', from_port=8200, to_port=8200, cidr_blocks=[vpc.cidr_block]
+            protocol='tcp', from_port=8600, to_port=8600, cidr_blocks=[vpc.cidr_block]
+        ),
+        pulumi_aws.ec2.SecurityGroupIngressArgs(
+            protocol='udp', from_port=8600, to_port=8600, cidr_blocks=[vpc.cidr_block]
         ),
         pulumi_aws.ec2.SecurityGroupIngressArgs(
             protocol='tcp', from_port=8500, to_port=8500, cidr_blocks=[vpc.cidr_block]
+        ),
+        pulumi_aws.ec2.SecurityGroupIngressArgs(
+            protocol='tcp', from_port=8301, to_port=8301, cidr_blocks=[vpc.cidr_block]
+        ),
+        pulumi_aws.ec2.SecurityGroupIngressArgs(
+            protocol='udp', from_port=8301, to_port=8301, cidr_blocks=[vpc.cidr_block]
+        ),
+        pulumi_aws.ec2.SecurityGroupIngressArgs(
+            protocol='tcp', from_port=8300, to_port=8300, cidr_blocks=[vpc.cidr_block]
         )
     ]
 )
 
-consul_server = pulumi_aws.ec2.Instance(
-    'consul-server',
-    instance_type='t2.micro',
-    ami=aws_ami.id,
-    key_name='aws-keypair',
-    vpc_security_group_ids=[
-        consul_server_sg.id
-    ]
-)
+with open('./cloud-init/server-agent.yml') as f:
+    user_data = f.read()
+
+
+for i in range(1, 4):
+    consul_server = pulumi_aws.ec2.Instance(
+        f'consul-server-{i}',
+        instance_type='t2.micro',
+        ami=aws_ami.id,
+        key_name='aws-keypair',
+        vpc_security_group_ids=[
+            consul_server_sg.id
+        ],
+        tags={
+            'Name': f'consul-server-{i}',
+            'Consul': 'consul-server'
+        },
+        user_data=user_data,
+    )
